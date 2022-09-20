59c2956e0b7d9776db3512c6146815bb9597d6df
CheYH <08822407d@gmail.com>
2022-09-20 06:55:55 -0700

    creat

--- genChkReport/Form1.Designer.cs
+++ genChkReport/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace genChkReport
 {
-	partial class Form1
+	partial class frmMain
 	{
 		/// <summary>
 		/// 必需的设计器变量。
@@ -28,13 +28,96 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.btn_cancel = new System.Windows.Forms.Button();
+			this.btn_start = new System.Windows.Forms.Button();
+			this.tbx_outputDIR = new System.Windows.Forms.TextBox();
+			this.btn_outputDIR = new System.Windows.Forms.Button();
+			this.tbx_inputDIR = new System.Windows.Forms.TextBox();
+			this.btn_inputDIR = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btn_cancel
+			// 
+			this.btn_cancel.Location = new System.Drawing.Point(336, 114);
+			this.btn_cancel.Name = "btn_cancel";
+			this.btn_cancel.Size = new System.Drawing.Size(108, 27);
+			this.btn_cancel.TabIndex = 13;
+			this.btn_cancel.Text = "取消";
+			this.btn_cancel.UseVisualStyleBackColor = true;
+			this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+			// 
+			// btn_start
+			// 
+			this.btn_start.Location = new System.Drawing.Point(156, 114);
+			this.btn_start.Name = "btn_start";
+			this.btn_start.Size = new System.Drawing.Size(108, 27);
+			this.btn_start.TabIndex = 12;
+			this.btn_start.Text = "开始处理";
+			this.btn_start.UseVisualStyleBackColor = true;
+			this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+			// 
+			// tbx_outputDIR
+			// 
+			this.tbx_outputDIR.AllowDrop = true;
+			this.tbx_outputDIR.Location = new System.Drawing.Point(156, 65);
+			this.tbx_outputDIR.Name = "tbx_outputDIR";
+			this.tbx_outputDIR.Size = new System.Drawing.Size(434, 25);
+			this.tbx_outputDIR.TabIndex = 11;
+			// 
+			// btn_outputDIR
+			// 
+			this.btn_outputDIR.Location = new System.Drawing.Point(12, 65);
+			this.btn_outputDIR.Name = "btn_outputDIR";
+			this.btn_outputDIR.Size = new System.Drawing.Size(126, 25);
+			this.btn_outputDIR.TabIndex = 10;
+			this.btn_outputDIR.Text = "选择输出文件夹";
+			this.btn_outputDIR.UseVisualStyleBackColor = true;
+			this.btn_outputDIR.Click += new System.EventHandler(this.btn_outputDIR_Click);
+			// 
+			// tbx_inputDIR
+			// 
+			this.tbx_inputDIR.AllowDrop = true;
+			this.tbx_inputDIR.Location = new System.Drawing.Point(156, 12);
+			this.tbx_inputDIR.Name = "tbx_inputDIR";
+			this.tbx_inputDIR.Size = new System.Drawing.Size(434, 25);
+			this.tbx_inputDIR.TabIndex = 9;
+			// 
+			// btn_inputDIR
+			// 
+			this.btn_inputDIR.Location = new System.Drawing.Point(12, 12);
+			this.btn_inputDIR.Name = "btn_inputDIR";
+			this.btn_inputDIR.Size = new System.Drawing.Size(126, 25);
+			this.btn_inputDIR.TabIndex = 8;
+			this.btn_inputDIR.Text = "选择输入文件夹";
+			this.btn_inputDIR.UseVisualStyleBackColor = true;
+			this.btn_inputDIR.Click += new System.EventHandler(this.btn_inputDIR_Click);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(603, 155);
+			this.Controls.Add(this.btn_cancel);
+			this.Controls.Add(this.btn_start);
+			this.Controls.Add(this.tbx_outputDIR);
+			this.Controls.Add(this.btn_outputDIR);
+			this.Controls.Add(this.tbx_inputDIR);
+			this.Controls.Add(this.btn_inputDIR);
+			this.Name = "frmMain";
+			this.Text = "生成检查报告";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btn_cancel;
+		private System.Windows.Forms.Button btn_start;
+		private System.Windows.Forms.TextBox tbx_outputDIR;
+		private System.Windows.Forms.Button btn_outputDIR;
+		private System.Windows.Forms.TextBox tbx_inputDIR;
+		private System.Windows.Forms.Button btn_inputDIR;
 	}
 }
 
--- genChkReport/Form1.cs
+++ genChkReport/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,71 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Utils;
+
 namespace genChkReport
 {
-	public partial class Form1 : Form
+	public partial class frmMain : Form
 	{
-		public Form1()
+		public frmMain()
 		{
 			InitializeComponent();
+
+			//tbx_inputDIR.Text = "‪C:\\Users\\cheyh\\Desktop\\testdata\\5米DEM检查记录\\检查记录";
+			//tbx_outputDIR.Text = "C:\\Users\\cheyh\\Desktop\\testdata\\5米DEM检查记录\\输出报告";
+		}
+
+		private void btn_inputDIR_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd =new FolderBrowserDialog();
+			fbd.Description = "请选择文件夹";
+			if (fbd.ShowDialog() == DialogResult.OK)
+			{
+				string dir = fbd.SelectedPath;
+				if (dir != null)
+				{
+					tbx_inputDIR.Text = dir;
+				}
+			}
+		}
+
+		private void btn_outputDIR_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd =new FolderBrowserDialog();
+			fbd.Description = "请选择文件夹";
+			if (fbd.ShowDialog() == DialogResult.OK)
+			{
+				string dir = fbd.SelectedPath;
+				if (dir != null)
+				{
+					tbx_outputDIR.Text = dir;
+				}
+			}
+		}
+
+		private void btn_cancel_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void btn_start_Click(object sender, EventArgs e)
+		{
+			string input_dir = tbx_inputDIR.Text;
+			string output_dir = tbx_outputDIR.Text;
+
+			if (!Directory.Exists(input_dir))
+			{
+				MessageBox.Show("错误：输入文件夹不存在");
+				return;
+			}
+			if (!Directory.Exists(output_dir))
+			{
+				MessageBox.Show("错误：输出文件夹不存在");
+				return;
+			}
+
+			List<string> excel_files = Utils.FIO.traverseSearchFile_Ext(input_dir, "xls");
+			excel_files = excel_files.Union(Utils.FIO.traverseSearchFile_Ext(input_dir, "xlsx")).ToList<string>();
 		}
 	}
 }
